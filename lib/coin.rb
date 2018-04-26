48b163d9c8127e32145c7ec39afb4cf2ab97ebe1
sunaliseth <sunaliseth01@gmail.com>
2018-04-26 08:51:09 +0100

    Coin values can only be of certain denominations, else raise error. Test passes

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,9 +1,11 @@
 class Coin
 
+VALUES_ACCEPTED = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, 2.00]
+
   attr_reader :value
 
   def initialize(value)
+    raise TypeError, 'Only 1p, 2p, 5p, 10p, 20p, 50p, £1, £2 accepeted' unless VALUES_ACCEPTED.include?(value)
     @value = value
   end
-
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Coin do
     coin = Coin.new(0.02)
     expect(coin.value).to eq(0.02)
   end
+
+  it "can only have values of 1p, 2p, 5p, 10p, 20p, 50p, £1, £2" do
+    expect{Coin.new(0.03)}.to raise_error("Only 1p, 2p, 5p, 10p, 20p, 50p, £1, £2 accepeted")
+  end
 end
